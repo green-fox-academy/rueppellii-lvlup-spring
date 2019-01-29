cbf017dd2cc801a30fb4131da16ba0d1d4d58d3c
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-01-29 13:07:07 +0100

    RUELVLS-9 created HeartbeatController and test

---
+++ src/main/java/com/greenfox/lvlup/controller/HeartbeatController.java
@@ -0,0 +1,22 @@
+package com.greenfox.lvlup.controller;
+
+import com.greenfox.lvlup.exception.GeneralException;
+import com.greenfox.lvlup.model.SuccessfulQuery;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeartbeatController {
+    @GetMapping(value = "/heartbeat",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<?> getHeartbeat(@RequestHeader(value = "userTokenAuth", required = false) String token) throws Exception {
+        if (token == null || token.equals("")) {
+            throw new GeneralException("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(new SuccessfulQuery("Ok"), HttpStatus.OK);
+    }
+}
