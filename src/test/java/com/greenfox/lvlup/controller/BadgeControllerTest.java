1833163c706fd07fcd299df6abcffdc2578cfed2
Adel Biro <b.adel93@gmail.com>
2019-01-29 12:00:26 +0100

    moved stringify method to util package

--- src/test/java/com/greenfox/lvlup/controller/BadgeControllerTest.java
+++ src/test/java/com/greenfox/lvlup/controller/BadgeControllerTest.java
@@ -1,8 +1,5 @@
 package com.greenfox.lvlup.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.greenfox.lvlup.util.Converter.stringify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,9 +39,5 @@ public class BadgeControllerTest {
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isUnauthorized())
         .andExpect(jsonPath("$.error").value("Unauthorized"));
-  }
-
-  private String stringify(Object object) throws JsonProcessingException {
-    return new ObjectMapper().writeValueAsString(object);
   }
 }
