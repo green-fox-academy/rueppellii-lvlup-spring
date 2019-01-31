40ae8b7341ceab5fc8588574a940a9d6be846bcb
Mark Palotas <ppmark@gmail.com>
2019-01-31 23:09:57 +0100

    oauth reverted to autoconfig, still keeps redirecting back to google auth page

--- src/main/java/com/greenfox/lvlup/LvlupApplication.java
+++ src/main/java/com/greenfox/lvlup/LvlupApplication.java
@@ -2,10 +2,12 @@ package com.greenfox.lvlup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 // import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class LvlupApplication {
 
   public static void main(String[] args) {
