72cdcbfed3f9ba640af8fe9e91d3d88c3b93d1af
Mark Palotas <ppmark@gmail.com>
2019-02-01 17:53:28 +0100

    still not working

--- src/main/java/com/greenfox/lvlup/config/OauthSecurityConfig.java
+++ src/main/java/com/greenfox/lvlup/config/OauthSecurityConfig.java
@@ -2,20 +2,22 @@ package com.greenfox.lvlup.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableOAuth2Sso
-public class OauthSecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Order(1)
+public class OauthSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//          .antMatcher("/auth")
+          .antMatcher("/auth")
           .authorizeRequests()
-             .antMatchers("/authed")
-             .permitAll()
+//             .antMatchers("/authed")
+//             .permitAll()
           .anyRequest()
              .authenticated();
     }
