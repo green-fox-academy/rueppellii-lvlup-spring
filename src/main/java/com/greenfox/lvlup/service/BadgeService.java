50b9f1e10c849797a5474f8d6b24058e864e754d
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-02-07 15:56:02 +0100

    refactored badges model using ModelMapper

--- src/main/java/com/greenfox/lvlup/service/BadgeService.java
+++ src/main/java/com/greenfox/lvlup/service/BadgeService.java
@@ -1,9 +1,9 @@
 package com.greenfox.lvlup.service;
 
-import com.greenfox.lvlup.model.entity.BadgeLevel;
 import com.greenfox.lvlup.model.dto.BadgeDTO;
 import com.greenfox.lvlup.model.entity.Badge;
 import com.greenfox.lvlup.repositrory.BadgeRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,33 +13,22 @@ import java.util.List;
 @Service
 public class BadgeService {
     private BadgeRepository badgeRepository;
-    private BadgeLevelService badgeLevelService;
+    private ModelMapper modelMapper;
 
 
     @Autowired
-    public BadgeService(BadgeRepository badgeRepository, BadgeLevelService badgeLevelService) {
+    public BadgeService(BadgeRepository badgeRepository, ModelMapper modelMapper) {
         this.badgeRepository = badgeRepository;
-        this.badgeLevelService = badgeLevelService;
+        this.modelMapper = modelMapper;
     }
-    public List<BadgeDTO> getDTOListFromBadge() {
+
+    public List<BadgeDTO> convertBadgeToBadgeDTO() {
         List<BadgeDTO> badgeDTOs = new ArrayList<>();
         List<Badge> badges = badgeRepository.findAll();
         for (Badge badge : badges) {
-            badgeDTOs.add(getDTOfromBadge(badge));
+            BadgeDTO badgeDTO = modelMapper.map(badge, BadgeDTO.class);
+            badgeDTOs.add(badgeDTO);
         }
         return badgeDTOs;
-    }
-
-    public BadgeDTO getDTOfromBadge(Badge badge) {
-        BadgeDTO badgeDTO = new BadgeDTO();
-        badgeDTO.version = badge.getVersion();
-        badgeDTO.name = badge.getName();
-        badgeDTO.tag = badge.getTag();
-        badgeDTO.levels = new ArrayList<>();
-        List<BadgeLevel> levels = badge.getLevels();
-        for (BadgeLevel item : levels) {
-            badgeDTO.levels.add(badgeLevelService.getDTOfromBadgeLevel(item));
-        }
-        return badgeDTO;
     }
 }
