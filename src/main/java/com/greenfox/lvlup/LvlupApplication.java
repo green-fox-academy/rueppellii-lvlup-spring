b41e586fd5c768e2a131cabfdfcd43a68a5ef7da
Diana <dianafbernardo@gmail.com>
2019-02-08 14:15:31 +0100

    Deleted fake data

--- src/main/java/com/greenfox/lvlup/LvlupApplication.java
+++ src/main/java/com/greenfox/lvlup/LvlupApplication.java
@@ -14,38 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LvlupApplication implements CommandLineRunner {
 
-  ArchetypeRepository archetypeRepository;
-  BadgeLevelRepository levelRepository;
-  BadgeRepository badgeRepository;
-
-  @Autowired
-  public LvlupApplication(ArchetypeRepository archetypeRepository, BadgeLevelRepository levelRepository, BadgeRepository badgeRepository) {
-    this.archetypeRepository = archetypeRepository;
-    this.levelRepository = levelRepository;
-    this.badgeRepository = badgeRepository;
-  }
-
   public static void main(String[] args) {
         SpringApplication.run(LvlupApplication.class, args);
     }
 
   @Override
   public void run(String... args) throws Exception {
-
-    Badge badge1 = new Badge("3.2", "testEnglish", "TESTING");
-    Badge badge2 = new Badge("2.2", "testPresentation skills", "prezi");
-    Badge badge3 = new Badge("1.2", "testCommunication skills", "communication");
-    badgeRepository.save(badge1);
-    badgeRepository.save(badge2);
-    badgeRepository.save(badge3);
-
-    Archetype arh1 = new Archetype("Junior mentor");
-    Archetype arh2 = new Archetype("Senior mentor");
-    archetypeRepository.save(arh1);
-    archetypeRepository.save(arh2);
-
-    levelRepository.save(new BadgeLevel(5, "Category 5", badge2, arh1, arh2));
-    levelRepository.save(new BadgeLevel(3, "Category 3", badge2, arh1));
   }
 }
 
