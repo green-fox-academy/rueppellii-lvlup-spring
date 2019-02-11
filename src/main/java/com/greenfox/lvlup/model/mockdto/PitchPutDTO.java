2144aa1f8a3634232430e8a63b798b9cc6af4e16
Mark Palotas <ppmark@gmail.com>
2019-02-11 08:08:08 +0100

    corrected typos in PitchPutDTO

--- src/main/java/com/greenfox/lvlup/model/mockdto/PitchPutDTO.java
+++ src/main/java/com/greenfox/lvlup/model/mockdto/PitchPutDTO.java
@@ -6,9 +6,9 @@ import javax.validation.constraints.NotNull;
 public class PitchPutDTO {
     @NotEmpty(message = "Name of pitcher is required.")
     String pitcherName;
-    @NotNull(message = "Badge level is required.")
+    @NotEmpty(message = "Badge level is required.")
     String badgeName;
-    @NotNull(message = "New status is required.")
+    @NotEmpty(message = "New status is required.")
     String newStatus;
     @NotEmpty(message = "New message is required.")
     String newMessage;
