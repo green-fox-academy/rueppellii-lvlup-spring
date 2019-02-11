116167141887bfa4d49ffa7ec5cf5a610100dc54
Mark Palotas <ppmark@gmail.com>
2019-02-11 10:17:39 +0100

    create pitch PUT endpoint

--- src/main/java/com/greenfox/lvlup/controller/PitchController.java
+++ src/main/java/com/greenfox/lvlup/controller/PitchController.java
@@ -5,6 +5,7 @@ import com.greenfox.lvlup.exception.GeneralException;
 import com.greenfox.lvlup.exception.SuccessfulQuery;
 import javax.validation.Valid;
 
+import com.greenfox.lvlup.model.mockdto.PitchPutDTO;
 import com.greenfox.lvlup.model.mockdto.PitchSetDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -30,6 +31,15 @@ public class PitchController {
       throw new GeneralException("Unauthorized", HttpStatus.UNAUTHORIZED);
     }
     return new ResponseEntity(new SuccessfulQuery("Success"), HttpStatus.CREATED);
+  }
+
+  @PutMapping(value = "/pitch", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> modifyPitch(@RequestHeader(value = "userTokenAuth") String token,
+                                            @Valid @RequestBody PitchPutDTO pitchPutDTO) throws Exception {
+    if (token.isEmpty()) {
+      throw new GeneralException("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
+    return new ResponseEntity<>(new SuccessfulQuery("Success"), HttpStatus.CREATED);
   }
 
 }
--- src/main/java/com/greenfox/lvlup/model/mockdto/PitchPutDTO.java
+++ src/main/java/com/greenfox/lvlup/model/mockdto/PitchPutDTO.java
@@ -1,7 +1,6 @@
 package com.greenfox.lvlup.model.mockdto;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 public class PitchPutDTO {
     @NotEmpty(message = "Name of pitcher is required.")
@@ -12,4 +11,47 @@ public class PitchPutDTO {
     String newStatus;
     @NotEmpty(message = "New message is required.")
     String newMessage;
+
+
+    public PitchPutDTO(String pitcherName, String badgeName, String newStatus, String newMessage) {
+        this.pitcherName = pitcherName;
+        this.badgeName = badgeName;
+        this.newStatus = newStatus;
+        this.newMessage = newMessage;
+    }
+
+    public PitchPutDTO() {
+    }
+
+    public String getPitcherName() {
+        return pitcherName;
+    }
+
+    public void setPitcherName(String pitcherName) {
+        this.pitcherName = pitcherName;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
+
+    public void setBadgeName(String badgeName) {
+        this.badgeName = badgeName;
+    }
+
+    public String getNewStatus() {
+        return newStatus;
+    }
+
+    public void setNewStatus(String newStatus) {
+        this.newStatus = newStatus;
+    }
+
+    public String getNewMessage() {
+        return newMessage;
+    }
+
+    public void setNewMessage(String newMessage) {
+        this.newMessage = newMessage;
+    }
 }
