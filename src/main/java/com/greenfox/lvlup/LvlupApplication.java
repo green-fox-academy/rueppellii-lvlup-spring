a72f637f2aa303d80d43eb32e00c00e1c49fb225
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-02-11 16:10:40 +0100

    final modifications in order to create seeded data for users, badges, badlevels and archetypes

--- src/main/java/com/greenfox/lvlup/LvlupApplication.java
+++ src/main/java/com/greenfox/lvlup/LvlupApplication.java
@@ -1,9 +1,13 @@
 package com.greenfox.lvlup;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
 
 @SpringBootApplication
 public class LvlupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LvlupApplication.class, args);
+    }
 }
-
-
