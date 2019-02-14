fe285347db39c8927e2c8ebffe715ed036881c46
Adel Biro <b.adel93@gmail.com>
2019-02-14 10:48:08 +0100

    trying to fix testcases

--- src/test/java/com/greenfox/lvlup/controller/AdminControllerTest.java
+++ src/test/java/com/greenfox/lvlup/controller/AdminControllerTest.java
@@ -1,15 +1,22 @@
 package com.greenfox.lvlup.controller;
 
 import com.greenfox.lvlup.model.dto.BadgeDTO;
+import com.greenfox.lvlup.security.JwtAuthenticationProvider;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static com.greenfox.lvlup.util.Converter.stringify;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -19,17 +26,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
   @RunWith(SpringRunner.class)
   @WebMvcTest(AdminController.class)
   public class AdminControllerTest {
-    String token = "TestToken123";
+    String token = "a valid token";
     BadgeDTO validBadgeDto = new BadgeDTO("2.3", "Test badge", "general");
     BadgeDTO invalidBadgeDto = new BadgeDTO("2.3", "", "general");
+
+    private JwtAuthenticationProvider provider;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+      provider = new JwtAuthenticationProvider();
+      mockMvc = MockMvcBuilders
+          .webAppContextSetup(context)
+          .apply(springSecurity())
+          .build();
+    }
+
     @Test
     public void addBadgeValidRequestReturns201Created() throws Exception {
       this.mockMvc.perform(post("/admin/add")
-          .header("userTokenAuth", token)
+          .header("Authentication", "Bearer " + token)
           .contentType(MediaType.APPLICATION_JSON)
           .content(stringify(validBadgeDto)))
           .andDo(print())
