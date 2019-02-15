a42234918eec684e60f7f0fed6ff24a3307cd7b4
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-02-15 16:12:23 +0100

    x

--- src/main/java/com/greenfox/lvlup/config/SetupInitialData.java
+++ src/main/java/com/greenfox/lvlup/config/SetupInitialData.java
@@ -57,7 +57,7 @@ public class SetupInitialData implements CommandLineRunner {
         generateBadgelevelsWithUsersAndArchetypes();
         generateReviews();
         generatePitches();
-
+        setPitchesInReviews();
         saveAll();
     }
 
@@ -91,35 +91,35 @@ public class SetupInitialData implements CommandLineRunner {
                 new BadgeLevel(1, "I can reliably improve processes across the organization.",
                         badges.get(0), users.get(0), users.get(1), users.get(3), users.get(5)),
                 new BadgeLevel(2, "I can introduce processes that are new to the company and implement them.",
-                        badges.get(0), archetypes.get(0)));
-        new BadgeLevel(3, "I can come up with brand new processes that have high impact on the company",
-                badges.get(0), archetypes.get(1), archetypes.get(2));
-        new BadgeLevel(0, "I can see through processes and propose relevant and doable ideas for improvement.",
-                badges.get(0), users.get(6));
-        new BadgeLevel(1, "I can present/facilitate sessions in English yet not always fluently.",
-                badges.get(1), users.get(6), users.get(4), users.get(1), users.get(2));
-        new BadgeLevel(2, "I can confidently present/facilitate sessions in English.",
-                badges.get(1), users.get(0), users.get(3), users.get(5));
-        new BadgeLevel(3, "I can express and understand nuanced opinions, needs and feelings.",
-                badges.get(1), archetypes.get(0));
-        new BadgeLevel(4, "I can negotiate with clients and partners, using sophisticated, grammaticaly correct English.",
-                badges.get(1), archetypes.get(1), archetypes.get(2));
-        new BadgeLevel(1, "I usually receive feedback without interrupting, getting defensive, trying to find excuses, lashing out.",
-                badges.get(2), users.get(1), users.get(3), users.get(5));
-        new BadgeLevel(2, "I proactively seek feedback on projects or my general .",
-                badges.get(2), users.get(4), users.get(6));
-        new BadgeLevel(2, "When you give me feedback, I help yout get specific on: what I did, why could we do about it.",
-                badges.get(2), archetypes.get(0), archetypes.get(1));
-        new BadgeLevel(2, "I am a role-model for how to receive and process feedback.",
-                badges.get(2), archetypes.get(2));
-        new BadgeLevel(1, "I proactively give specific, easy to understand observations, and make a request to you.",
-                badges.get(3), archetypes.get(0));
-        new BadgeLevel(0, "When I have a problem, I usually let you know by expressing it clearly instead of keeping it to myself.",
-                badges.get(3), users.get(1), users.get(2), users.get(3));
-        new BadgeLevel(2, "I decribe my feelings and needs in conflict situations with most of my team mates.",
-                badges.get(3), archetypes.get(1));
-        new BadgeLevel(3, "I am confident in describing my feelings and needs considering the feedback receivers feelings and needs " +
-                "and suggest a constructive solution", badges.get(3), archetypes.get(2));
+                        badges.get(0), archetypes.get(0)),
+                new BadgeLevel(3, "I can come up with brand new processes that have high impact on the company",
+                        badges.get(0), archetypes.get(1), archetypes.get(2)),
+                new BadgeLevel(0, "I can see through processes and propose relevant and doable ideas for improvement.",
+                        badges.get(0), users.get(6)),
+                new BadgeLevel(1, "I can present/facilitate sessions in English yet not always fluently.",
+                        badges.get(1), users.get(6), users.get(4), users.get(1), users.get(2)),
+                new BadgeLevel(2, "I can confidently present/facilitate sessions in English.",
+                        badges.get(1), users.get(0), users.get(3), users.get(5)),
+                new BadgeLevel(3, "I can express and understand nuanced opinions, needs and feelings.",
+                        badges.get(1), archetypes.get(0)),
+                new BadgeLevel(4, "I can negotiate with clients and partners, using sophisticated, grammaticaly correct English.",
+                        badges.get(1), archetypes.get(1), archetypes.get(2)),
+                new BadgeLevel(1, "I usually receive feedback without interrupting, getting defensive, trying to find excuses, lashing out.",
+                        badges.get(2), users.get(1), users.get(3), users.get(5)),
+                new BadgeLevel(2, "I proactively seek feedback on projects or my general .",
+                        badges.get(2), users.get(4), users.get(6)),
+                new BadgeLevel(2, "When you give me feedback, I help yout get specific on: what I did, why could we do about it.",
+                        badges.get(2), archetypes.get(0), archetypes.get(1)),
+                new BadgeLevel(2, "I am a role-model for how to receive and process feedback.",
+                        badges.get(2), archetypes.get(2)),
+                new BadgeLevel(1, "I proactively give specific, easy to understand observations, and make a request to you.",
+                        badges.get(3), archetypes.get(0)),
+                new BadgeLevel(0, "When I have a problem, I usually let you know by expressing it clearly instead of keeping it to myself.",
+                        badges.get(3), users.get(1), users.get(2), users.get(3)),
+                new BadgeLevel(2, "I decribe my feelings and needs in conflict situations with most of my team mates.",
+                        badges.get(3), archetypes.get(1)),
+                new BadgeLevel(3, "I am confident in describing my feelings and needs considering the feedback receivers feelings and needs " +
+                        "and suggest a constructive solution", badges.get(3), archetypes.get(2)));
     }
 
     public void generateReviews() {
@@ -149,14 +149,17 @@ public class SetupInitialData implements CommandLineRunner {
                         "pls give me more money", users.get(3), badges.get(0)),
                 new Pitch(new Date(), 4, 5, "I am confident in describing my feelings and needs considering the feedback " +
                         "receivers feelings and suggest a constructive solution", users.get(4), badges.get(1)));
+    }
+
+    private void setPitchesInReviews() {
         reviews.get(0).setPitch(pitches.get(0));
-        /*review2.setPitch(pitch1);
-        review3.setPitch(pitch2);
-        review4.setPitch(pitch2);
-        review5.setPitch(pitch3);
-        review6.setPitch(pitch5);
-        review7.setPitch(pitch6);
-        review8.setPitch(pitch4);*/
+        reviews.get(1).setPitch(pitches.get(0));
+        reviews.get(2).setPitch(pitches.get(1));
+        reviews.get(3).setPitch(pitches.get(1));
+        reviews.get(4).setPitch(pitches.get(2));
+        reviews.get(5).setPitch(pitches.get(4));
+        reviews.get(6).setPitch(pitches.get(5));
+        reviews.get(7).setPitch(pitches.get(3));
     }
 
     private void saveAll() {
@@ -167,32 +170,6 @@ public class SetupInitialData implements CommandLineRunner {
         pitches.forEach(p -> pitchRepository.save(p));
         reviews.forEach(r -> reviewRepository.save(r));
     }
-
-/*    public void setUpReviewsAndPitches() {
-
-        Pitch pitch1 = new Pitch(new Date(), 2, 3, "I was working abroad for six years, so I can speak english very well. Pls improve my badge level to 3.", user1, badge2);
-        Pitch pitch2 = new Pitch(new Date(), 1, 5, "I can see through processes and propose relevant and doable ideas for improvement. I can create improved definition / accountibility / documentation and communicate it to the team", user1, badge1);
-        Pitch pitch3 = new Pitch(new Date(), 1, 3, "I proactively seek feedback on projects or my general work and choose how to integrate it.", user2, badge3);
-        Pitch pitch4 = new Pitch(new Date(), 4, 5, "When I have a problem, I usually let you know by expressing it clearly instead of keeping to myself", user3, badge4);
-        Pitch pitch5 = new Pitch(new Date(), 2, 3, "I improved in React, Redux, basic JS, NodeJS, Express and in LowDB, pls give me more money", user4, badge1);
-        Pitch pitch6 = new Pitch(new Date(), 4, 5, "I am confident in describing my feelings and needs considering the feedback receivers feelings and suggest a constructive solution", user5, badge2);
-        pitchRepository.save(pitch1);
-        pitchRepository.save(pitch2);
-        pitchRepository.save(pitch3);
-        pitchRepository.save(pitch4);
-        pitchRepository.save(pitch5);
-        pitchRepository.save(pitch6);
-
-
-        review1.setPitch(pitch1);
-        review2.setPitch(pitch1);
-        review3.setPitch(pitch2);
-        review4.setPitch(pitch2);
-        review5.setPitch(pitch3);
-        review6.setPitch(pitch5);
-        review7.setPitch(pitch6);
-        review8.setPitch(pitch4);
-    }*/
 }
 
 
