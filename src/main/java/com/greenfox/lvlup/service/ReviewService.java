1f532855e60f0d40622e3184e3ff44d02c001a02
Mark Palotas <ppmark@gmail.com>
2019-02-26 14:13:48 +0100

    complete ReviewService

--- src/main/java/com/greenfox/lvlup/service/ReviewService.java
+++ src/main/java/com/greenfox/lvlup/service/ReviewService.java
@@ -1,11 +1,15 @@
 package com.greenfox.lvlup.service;
 
+import com.greenfox.lvlup.exception.GeneralException;
 import com.greenfox.lvlup.model.dto.pitches.ReviewDTO;
 import com.greenfox.lvlup.model.entity.Review;
 import com.greenfox.lvlup.repositrory.ReviewRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ReviewService {
@@ -18,14 +22,21 @@ public class ReviewService {
         this.mapper = mapper;
     }
 
-    public void saveReview(ReviewDTO reviewDto) {
-
+    public void updateReview(ReviewDTO reviewDto) throws GeneralException {
+        reviewRepository.save(convertReviewDTOToEntityForPut(reviewDto));
     }
 
-    public Review convertReviewDTOToEntity(ReviewDTO reviewDto) {
-        Review review = mapper.map(reviewDto, Review.class);
+    public Review convertReviewDTOToEntityForPut(ReviewDTO reviewDto) throws GeneralException {
+        Review review = findById(reviewDto.getId());
+        mapper.map(reviewDto, review);
         return review;
     }
 
-
+    public Review findById(Long id) throws GeneralException {
+        Optional<Review> review = reviewRepository.findById(id);
+        if (review.isPresent()) {
+            return review.get();
+        }
+        throw new GeneralException("Review does not exist", HttpStatus.NOT_FOUND);
+    }
 }
