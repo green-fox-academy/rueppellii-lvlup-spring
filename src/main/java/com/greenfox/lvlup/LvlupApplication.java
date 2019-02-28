ad2473a2e9691218bec444c8eb6b52d65909ea39
Adel Biro <b.adel93@gmail.com>
2019-02-28 16:14:01 +0100

    refact main class

--- src/main/java/com/greenfox/lvlup/LvlupApplication.java
+++ src/main/java/com/greenfox/lvlup/LvlupApplication.java
@@ -9,25 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LvlupApplication implements CommandLineRunner {
 
-    @Value("${spring.datasource.url}")
-    private String dbname;
+  @Value("${spring.datasource.url}")
+  private String dbname;
 
-    public static void main(String[] args) {
-        SpringApplication.run(LvlupApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LvlupApplication.class, args);
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("****************************" + dbname);
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("****************************" + dbname);
 
-
-
-    }
-
-
-
-
-
+  }
 }
 
 
