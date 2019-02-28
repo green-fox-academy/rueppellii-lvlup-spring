d5bac15f9de1e135e71d643072ce4bf207310a24
Adel Biro <b.adel93@gmail.com>
2019-02-28 14:08:33 +0100

    deleted unused class(customAuthExc) and unused exc. handler methods

--- src/main/java/com/greenfox/lvlup/exception/DefaultExceptionHandler.java
+++ src/main/java/com/greenfox/lvlup/exception/DefaultExceptionHandler.java
@@ -17,25 +17,8 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
     return new ResponseEntity<>(e.getErrorMessage(), e.getHttpStatus());
   }
 
-  @ExceptionHandler
-  public ResponseEntity<Object> handleAuthenticationException(Exception ex){
-    CustomAuthenticationException e = new CustomAuthenticationException(ex.getMessage(), HttpStatus.UNAUTHORIZED);
-    return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
-  }
-
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,                                                                HttpStatus status, WebRequest request) {
     return new ResponseEntity<>(ValidationErrorBuilder.fromBindingErrors(ex.getBindingResult()), HttpStatus.BAD_REQUEST);
   }
-
-//  @ExceptionHandler(ServletException.class)
-//  public ResponseEntity<Object> handleRuntimeException(ServletException e) {
-//    return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-//
-//  }
-//
-//  @ExceptionHandler(MalformedJwtException.class)
-//  public ResponseEntity<ErrorMessage> handleMalformedJwtException() {
-//    return new ResponseEntity<>(new ErrorMessage("Unauthorized"), HttpStatus.UNAUTHORIZED);
-//  }
 }
--- src/main/java/com/greenfox/lvlup/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/greenfox/lvlup/security/JwtAuthenticationTokenFilter.java
@@ -1,14 +1,7 @@
 package com.greenfox.lvlup.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.greenfox.lvlup.exception.CustomAuthenticationException;
-import com.greenfox.lvlup.exception.DefaultExceptionHandler;
 import com.greenfox.lvlup.exception.ErrorMessage;
-import com.greenfox.lvlup.exception.GeneralException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -24,10 +17,7 @@ import java.io.PrintWriter;
 
 public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
 
-  @Autowired
-  private DefaultExceptionHandler exceptionHandler;
-
-  public JwtAuthenticationTokenFilter () {
+  public JwtAuthenticationTokenFilter() {
     super("/api/**");
   }
 
@@ -54,21 +44,19 @@ public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessi
     chain.doFilter(request, response);
   }
 
-// this is new
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
     try {
-     super.doFilter(req, res, chain);
-   }
-   catch (RuntimeException e) {
-     ErrorMessage message = new ErrorMessage(e.getLocalizedMessage());
-     response.setStatus(401);
+      super.doFilter(req, res, chain);
+    } catch (RuntimeException e) {
+      ErrorMessage message = new ErrorMessage(e.getLocalizedMessage());
+      response.setStatus(401);
 
-     ObjectMapper mapper = new ObjectMapper();
-     PrintWriter out = res.getWriter();
-     out.print(mapper.writeValueAsString(message));
-     out.flush();
-   }
+      ObjectMapper mapper = new ObjectMapper();
+      PrintWriter out = res.getWriter();
+      out.print(mapper.writeValueAsString(message));
+      out.flush();
+    }
   }
 }
